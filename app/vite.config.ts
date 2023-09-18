fd80b35ef40c9e53fe7f2d6ef29cb42817ecb8ff
Alaister Young <a@alaisteryoung.com>
2023-09-19 00:56:26 +0200

    add html minifier

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteMinifyPlugin()],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src'),
