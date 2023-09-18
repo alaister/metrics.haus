b5ca3c58a66e209d3a6e2dafe7371430b70d1455
Alaister Young <a@alaisteryoung.com>
2023-09-19 00:06:13 +0200

    simplify

--- app/src/components/routes/Root.tsx
+++ app/src/components/routes/Root.tsx
@@ -4,10 +4,10 @@ import Header from '~/components/layout/Header'
 
 const Root = () => {
   return (
-    <div className="flex flex-col relative items-stretch bg-gray-50 min-h-screen-dvh font-sans">
+    <div className="flex flex-col relative items-stretch min-h-screen-dvh font-sans">
       <Header />
 
-      <main className="flex-1 container bg-white shadow-lg overflow-x-hidden pb-16 sm:pb-20 lg:pb-24">
+      <main className="flex-1 container overflow-x-hidden pt-8 pb-16 sm:pb-20 lg:pb-24">
         <Outlet />
       </main>
 
