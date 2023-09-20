46e62a075eb1f03961c1e4ba3b591f81e37d2ac8
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-09-20 23:44:15 +0200

    feat: add user avatar in the header (#37)

--- app/src/components/layout/Header.tsx
+++ app/src/components/layout/Header.tsx
@@ -1,35 +1,15 @@
 import { useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { toast } from '~/lib/hooks/use-toast'
 import supabase from '~/lib/supabase'
 import TeamSelector from '../teams/TeamSelector'
-import { Button } from '../ui/Button'
+import { Notifications } from '../notifications'
+import { UserAvatar } from '../UserAvatar'
 
 const Header = () => {
-  const [isLoading, setIsLoading] = useState(false)
   const [userStats, setUserStats] = useState<{
     num_data_points_created?: number
   }>({})
-  const navigate = useNavigate()
-
-  const signOut = async () => {
-    setIsLoading(true)
-
-    const { error } = await supabase.auth.signOut({ scope: 'local' })
-    if (error) {
-      toast({
-        variant: 'destructive',
-        title: 'Something went wrong',
-        description: error.message,
-      })
-      setIsLoading(false)
-      return
-    }
-
-    setIsLoading(false)
-    navigate('/sign-in', { replace: true })
-  }
-
   useEffect(() => {
     supabase
       .rpc('get_user_stats')
@@ -58,14 +38,10 @@ const Header = () => {
           <TeamSelector />
         </div>
         Points: {userStats.num_data_points_created ?? '...'}
-        <Button
-          variant="ghost"
-          onClick={signOut}
-          isLoading={isLoading}
-          disabled={isLoading}
-        >
-          Sign Out
-        </Button>
+        <div className="flex pr-6 gap-3">
+          <Notifications />
+          <UserAvatar />
+        </div>
       </div>
     </header>
   )
