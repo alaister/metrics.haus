5c02bab0386bcd8f982e2c3215186f836ece5203
Long Hoang <1732217+loong@users.noreply.github.com>
2023-09-20 22:23:02 +0200

    feat: add user stats

--- app/src/components/layout/Header.tsx
+++ app/src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from '~/lib/hooks/use-toast'
 import supabase from '~/lib/supabase'
@@ -7,6 +7,9 @@ import { Button } from '../ui/Button'
 
 const Header = () => {
   const [isLoading, setIsLoading] = useState(false)
+  const [userStats, setUserStats] = useState<{
+    num_data_points_created?: number
+  }>({})
   const navigate = useNavigate()
 
   const signOut = async () => {
@@ -27,6 +30,24 @@ const Header = () => {
     navigate('/sign-in', { replace: true })
   }
 
+  useEffect(() => {
+    supabase
+      .rpc('get_user_stats')
+      .single()
+      .then((resp) => {
+        const { data, error } = resp
+        if (error) {
+          toast({
+            variant: 'destructive',
+            title: 'Something went wrong',
+            description: error.message,
+          })
+          return
+        }
+        setUserStats(data)
+      })
+  }, [])
+
   return (
     <header className="sticky top-0 border-b bg-white z-10">
       <div className="flex h-16 justify-between items-center px-4">
@@ -34,10 +55,9 @@ const Header = () => {
           <h1 className="font-medium">
             <Link to="/">metrics.haus</Link>
           </h1>
-
           <TeamSelector />
         </div>
-
+        Points: {userStats.num_data_points_created ?? '...'}
         <Button
           variant="ghost"
           onClick={signOut}
