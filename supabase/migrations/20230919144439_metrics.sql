e8f96c74b2cc53ff7aae21fcd91f61582c9b8167
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2023-09-20 10:27:48 +0200

    feat: insert team member on identity insert (#15)

--- supabase/migrations/20230919144439_metrics.sql
+++ supabase/migrations/20230919144439_metrics.sql
@@ -1,11 +1,14 @@
 -- Enable the "timescaledb" extension
 create extension timescaledb;
 
+create type metric_interval as enum('minute', 'hour', 'day', 'week', 'month');
+
 create table
     public.metrics (
         "id" uuid primary key not null default gen_random_uuid (),
         "created_at" timestamp with time zone not null default now(),
         "updated_at" timestamp with time zone not null default now(),
+        interval metric_interval not null,
         name text not null,
         team_id uuid not null references public.teams ("id") on delete cascade on update cascade
     );
@@ -53,3 +56,28 @@ select
 create index ix_metric_data_point_metric on public.metrics_data_points (metric_id, time desc);
 
 create index ix_metric_team on public.metrics (team_id);
+
+create table
+    public.metrics_owners (
+        metric_id uuid not null references public.metrics ("id") on delete cascade on update cascade,
+        user_id uuid not null references public.profiles ("id") on delete cascade on update cascade,
+        "created_at" timestamp with time zone not null default now(),
+        primary key (metric_id, user_id)
+    );
+
+alter table public.metrics_owners enable row level security;
+
+create policy "user can manage metric owners from accessible metrics" on public.metrics_owners using (
+    exists (
+        select
+            1
+        from
+            public.metrics as m
+        where
+            metric_id = m.id
+    )
+);
+
+create index ix_metrics_owners_metric_id on public.metrics_owners (metric_id);
+
+create index ix_metrics_owners_user_id on public.metrics_owners (user_id);
