34dfa70baf56816b70cf4e68684e4733b26c09be
Alaister Young <a@alaisteryoung.com>
2023-09-20 18:51:00 +0200

    fix: MetricDetailsSkeleton breaking things

--- app/src/components/routes/MetricDetails.data.tsx
+++ app/src/components/routes/MetricDetails.data.tsx
@@ -1,6 +1,4 @@
 import { graphql } from 'relay-runtime'
-import SkeletonList from '../loading/SkeletonList'
-import { MetricDetailsSkeleton } from './MetricDetails'
 
 export const query = graphql`
   query MetricDetails_Query($nodeId: ID!) {
@@ -14,8 +12,4 @@ export const query = graphql`
   }
 `
 
-export const fallback = (
-  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-    <SkeletonList count={3} skeleton={MetricDetailsSkeleton} />
-  </div>
-)
+export const fallback = <div>LOADING TODO BETTER LOADER LATERRRR</div>
