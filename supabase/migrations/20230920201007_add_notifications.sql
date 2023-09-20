ce88452ffa3f59aba9ca9c79c01c9c2ec13697e0
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2023-09-20 23:34:22 +0200

    never

--- supabase/migrations/20230920201007_add_notifications.sql
+++ supabase/migrations/20230920201007_add_notifications.sql
@@ -1,27 +1,16 @@
-create table "public"."notifications" (
-    "id" uuid not null default gen_random_uuid(),
-    "created_at" timestamp with time zone default now(),
-    "text" text default ''::text,
-    "user_id" uuid not null
-);
-
+create table
+    "public"."notifications" (
+        "id" uuid not null primary key default gen_random_uuid (),
+        "created_at" timestamp with time zone default now(),
+        "text" text default ''::text,
+        "profile_id" uuid not null references public.profiles ("id") on delete set null on update cascade,
+        "metadata" JSONB
+    );
 
 alter table "public"."notifications" enable row level security;
 
-CREATE UNIQUE INDEX notifications_pkey ON public.notifications USING btree (id);
+create policy "user can see their own notifications" on "public"."notifications" as permissive for
+select
+    to authenticated using ((auth.uid () = profile_id));
 
-alter table "public"."notifications" add constraint "notifications_pkey" PRIMARY KEY using index "notifications_pkey";
-
-alter table "public"."notifications" add constraint "notifications_user_id_fkey" FOREIGN KEY (user_id) REFERENCES auth.users(id) ON DELETE CASCADE not valid;
-
-alter table "public"."notifications" validate constraint "notifications_user_id_fkey";
-
-create policy "user can see their own notifications"
-on "public"."notifications"
-as permissive
-for select
-to authenticated
-using ((auth.uid() = user_id));
-
-
-
+create policy "user can delete their own notifications" on "public"."notifications" as permissive for delete to authenticated using ((auth.uid () = profile_id));
