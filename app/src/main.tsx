ea4301a3b2c737f87132a04e027e54cf688dbbbe
Alaister Young <a@alaisteryoung.com>
2023-09-30 18:14:18 +1000

    only render router dev tools in dev mode

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,5 @@
 import { RouterProvider } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider as ReduxProvider } from 'react-redux'
 import { RelayEnvironmentProvider } from 'react-relay'
@@ -11,12 +10,22 @@ import { store } from './stores'
 
 import './index.css'
 
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null // Render nothing in production
+  : React.lazy(() =>
+      import('@tanstack/router-devtools').then((res) => ({
+        default: res.TanStackRouterDevtools,
+      })),
+    )
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ReduxProvider store={store}>
       <RelayEnvironmentProvider environment={environment}>
         <RouterProvider router={router} />
-        <TanStackRouterDevtools router={router} initialIsOpen={false} />
+        <Suspense>
+          <TanStackRouterDevtools router={router} initialIsOpen={false} />
+        </Suspense>
         <Toaster />
       </RelayEnvironmentProvider>
     </ReduxProvider>
