17301a938bc480d7250cc00b4591932abac9727d
Alaister Young <a@alaisteryoung.com>
2023-12-13 01:13:27 +1100

    add apikey to codegen

--- app/codegen.ts
+++ app/codegen.ts
@@ -4,8 +4,12 @@ import { addTypenameSelectionDocumentTransform } from '@graphql-codegen/client-p
 const SUPABASE_URL =
   process.env.NEXT_PUBLIC_SUPABASE_URL || 'http://localhost:54321'
 
+const API_KEY_QUERY_STRING = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  ? `?apikey=${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`
+  : ''
+
 const config: CodegenConfig = {
-  schema: `${SUPABASE_URL}/graphql/v1`,
+  schema: `${SUPABASE_URL}/graphql/v1${API_KEY_QUERY_STRING}`,
   documents: 'src/**/*.tsx',
   overwrite: true,
   ignoreNoDocuments: true,
